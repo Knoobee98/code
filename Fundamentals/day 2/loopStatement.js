7d7ac8b9088915fa09e17a580cb9133b406cca0c
knoobee98 <fdhuha17@gmail.com>
2022-10-26 11:55:19 +0700

    new file loopStatement

---
+++ Fundamentals/day 2/loopStatement.js
@@ -0,0 +1,10 @@
+//loop statement
+//while
+//while(condition){
+//    //code
+//}
+let inii = 1;
+while(inii < 3){ //1 < 3? true ---> 2 < 3? true ---> 3 < 3? false
+    console.log("pizza");
+    inii++;
+}
