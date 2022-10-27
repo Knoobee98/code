dc26fde3d32e65283e8692819d833b5983be8e36
knoobee98 <fdhuha17@gmail.com>
2022-10-27 15:12:54 +0700

    delete file

--- Fundamentals/day 3/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-let bebas = 5; //start condition
-
-// while(bebas <= 3){ //(bebas <= 1)---> swbuah kondisiyang harus terpenuhiagar code didalamnya jalan
-//     console.log("pizza");
-//     bebas++;
-// }
