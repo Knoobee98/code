576c8df19a9410de39e0b91d5a72e1d54ceb6b36
knoobee98 <fdhuha17@gmail.com>
2022-11-03 14:30:24 +0700

    segitiga fix

--- Fundamentals/day 8/segitiga.js
+++ Fundamentals/day 8/segitiga.js
@@ -1,9 +1,10 @@
 let star = "";
 let number = 1;
 for(let i = 0; i <= 5; i++){
-    for(let j = 0; j <= i; j-- ){
-        star += number;
-        number++;
+    for(let j = 0; j <= i; j++){
+        star += "* "
+        // star += number;
+        // number++;
     }
     star += "\n"
 }
