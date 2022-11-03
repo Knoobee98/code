a6c64dd9900a65acc82bfd55ff52296151966cde
knoobee98 <fdhuha17@gmail.com>
2022-11-03 14:22:39 +0700

    day 8 half day

---
+++ Fundamentals/day 8/segitiga.js
@@ -0,0 +1,10 @@
+let star = "";
+let number = 1;
+for(let i = 0; i <= 5; i++){
+    for(let j = 0; j <= i; j-- ){
+        star += number;
+        number++;
+    }
+    star += "\n"
+}
+console.log(star);
