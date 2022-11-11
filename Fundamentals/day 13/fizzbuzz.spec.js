01e6fa4c9b8882b7d97fb33d4f3bcc78ed73ba1f
Fauzan <55498447+Knoobee98@users.noreply.github.com>
2022-11-11 13:06:57 +0700

    day 13 add testing file

--- Fundamentals/day 13/fizzbuzz.spec.js
+++ Fundamentals/day 13/fizzbuzz.spec.js
@@ -1,9 +1,11 @@
 const {BuzzFizz, JumlahDeret} = require('./fizzbuzz');
 
 test("fizzBuzz testing", () => {
-    expect(BuzzFizz(15)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]);
-    expect(BuzzFizz(5)).toEqual([1, 2, "Fizz", 4, "Buzz"]);
-    expect(BuzzFizz(0)).toEqual([]);
+    expect(BuzzFizz(15)).toEqual(expect.arrayContaining([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]));
+    expect(BuzzFizz(5)).toEqual(expect.arrayContaining([1, 2, "Fizz", 4, "Buzz"]));
+    expect(BuzzFizz(2)).toEqual(expect.arrayContaining([1, 2]));
+    expect(BuzzFizz(1)).toEqual(expect.arrayContaining([1]));
+    expect(BuzzFizz(0)).toEqual(expect.arrayContaining([]));
 });
 
 test("Jumlah Deret testing", () => {
