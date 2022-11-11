3c30a2c7142163004f66e6ece0a21d0024bca086
Fauzan <55498447+Knoobee98@users.noreply.github.com>
2022-11-11 13:50:52 +0700

    add explanation file

---
+++ Fundamentals/day 12/explanation.js
@@ -0,0 +1,29 @@
+function validation(email, cb){
+    email = email.toString();
+    if(email.includes("@")){
+        return true;
+    } else {
+        return false;
+    }
+}
+function result(emailFromValidation){
+    return emailFromValidation;
+}
+console.log(validation("fdhuha@gmail.com"));
+
+let Validation = (email, cb) => {
+    email = email.toString();
+    if(email.includes("@")){
+        return cb(true);
+    } else {
+        return cb(false);
+    }
+}
+
+// let result = (emailFromValidation) => {
+//     return emailFromValidation.toString();
+// }
+
+validation("fdhuha@gmail.com", result = (emailFromValidation) => {
+    return emailFromValidation;
+});
