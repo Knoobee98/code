f1cb2f1672432045c8470e99ef7cd80e3fbb4553
Fauzan Naufal Ihsan Dhuha <fdhuha17@gmail.com>
2022-11-25 00:09:25 +0700

    login form done

--- Front-End/day 5/e-commerce/src/pages/loginPage/login.jsx
+++ Front-End/day 5/e-commerce/src/pages/loginPage/login.jsx
@@ -1,19 +1,39 @@
-
 let Login = () => {
-    return(
-        <div class='box-border h-100 w-50 p-25 border-2 border-shadow grid grid-rows-5'>
-            <h1 class='flex justify-center font-size-20 px-50'>Login</h1>
-            <div class='grid grid-row-3 flex justify-center'>
-                <form class='form-horizontal'>
-                    <input type='text' placeholder='Username*' class='border-2 border-black rounded-md px-4 py-3' />
-                    <br />
-                    <input type='password' placeholder='Password*' class='border-2 border-black rounded-md px-4' />
-                    <br />
-                    <button class='border-2 border-black rounded-full bg-green-500 '>Login</button>
-                </form>
-            </div>
+  return (
+    <div class="min-h-screen bg-gray-100 flex items-center">
+      <div class="container mx-auto max-w-md shadow-md hover:shadow-lg transition duration-300">
+        <h1 class="font-bold text-black">Login or create an account</h1>
+        <div class="py-12 p-10 bg-white rounded-xl">
+          <div class="mb-6">
+            <input
+              type="text"
+              class="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-green-400 rounded"
+              placeholder="Username or Email"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              class="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-green-400 rounded"
+              placeholder="Password"
+            />
+          </div>
+          <input
+            type="checkbox"
+            class="appearance-none checked:bg-blue-500 ..."
+          />
+          <label class="text-sm text-gray-600"> Always Signed in</label>
+          <br/>
+          <span class="text-sm text-gray-700 inline-block mt-4 hover:text-indigo-600 hover:underline hover:cursor-pointer transition duration-200">
+            Forgot Password?
+          </span>
+          <button class="w-full mt-6 text-black-50 font-bold bg-green-600 py-3 rounded-full hover:bg-green-500 transition duration-300 ">
+            Login
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login;   
+export default Login;
