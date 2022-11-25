57e4718a41f690de9d0de8c960376bef7e2d5e5c
Fauzan <55498447+Knoobee98@users.noreply.github.com>
2022-11-25 13:25:18 +0700

    day 7 network-js done

--- Front-End/day 7/network-js/src/App.js
+++ Front-End/day 7/network-js/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
+// import axios
+import axios from "axios";
+//linkApi
+import linkApi from "./supports/constants/linkApi";
 function App() {
+  const [data, setData] = useState([]);
+
+  let getData = async () => {
+    try {
+      let response = await axios.get(`${linkApi}/users`);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (data.length === 0) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button onClick={getData}>Get Data</button> */}
+      {data.map((value, index) => {
+        return (
+          <p key={index}>
+            {value.username} - {value.email} - {value.password}
+          </p>
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+
+//1. rendering ---> masuk conditional statement
+//2. didalam conditional statement dia mengecek state data, length === 0 atau >0?
+//3. jika length === 0, maka dia akan return loading
---
+++ Front-End/day 7/network-js/src/supports/constants/linkApi.js
@@ -0,0 +1,3 @@
+const linkApi = 'http://localhost:5000';
+
+export default linkApi;
