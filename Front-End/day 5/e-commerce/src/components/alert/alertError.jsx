54924613be909de5079dd821815c4c6e75ff9f23
Fauzan Naufal Ihsan Dhuha <fdhuha17@gmail.com>
2022-11-27 18:24:57 +0700

    update register page & login page

---
+++ Front-End/day 5/e-commerce/src/components/alert/alertError.jsx
@@ -0,0 +1,9 @@
+let AlertError = () => {
+    return (
+        <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+            <span className="font-medium">Error alert!</span> Change a few things up and try submitting again.
+        </div>
+    )
+}
+
+export default AlertError;
