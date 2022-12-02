676a72d6bb90f1ce66181b949a940b49fc40b8fb
Fauzan <55498447+Knoobee98@users.noreply.github.com>
2022-12-02 16:02:34 +0700

    day 12 done

--- Front-End/day 5/e-commerce/src/components/navbar/navbar.jsx
+++ Front-End/day 5/e-commerce/src/components/navbar/navbar.jsx
@@ -9,69 +9,72 @@ import { useLocation, Link } from "react-router-dom";
 let Navbar = (props) => {
   const location = useLocation();
   return (
-    <div className="">
-      <div className="navbar flex justify-between px-10 h-20 w-screen">
-        <div className="flex items-center">
-          <Link to="/">
-            <img
-              src="https://purwadhika.com/static/media/logopwdk-01.a09cf2fc.png"
-              alt="logo"
-              width="50px"
-              height="50px"
-            />
-          </Link>
-          {location.pathname === '/register' ? 
-          null 
-          : 
-            <>
-              <span className="pl-10 font-bold">
-                Cards
-                </span>
-              <span className="pl-10 font-bold">
-                <Link to='/menu'>
-                  Menu
-                  </Link>
-              </span>
-              <span className="pl-10 font-bold">
-                Gift
-                </span>
-            </>
-          }
-        </div>
-        <div className="flex items-center">
-          {location.pathname === "/register" ? null : (
-            <>
-              <div className="flex items-center">
-                <MdLocationOn />
-                <span className="mr-10 font-bold">Find a store</span>
-              </div>
-              {props.data.username ? 
-                <div className="flex items-center">
-                  <div className="font-bold">
-                  {props.data.username}
-                  </div>
-                  <div className="mt-1 ml-1">
-                    <CgProfile onClick={props.logout.onLogout}/>
-                  </div>
+    <div>
+            <div className='navbar flex px-10 w-100 justify-between'>
+                <div className='flex items-center'>
+                    <Link to='/'>
+                        <img src="https://purwadhika.com/static/media/logopwdk-01.a09cf2fc.png" width='50px' height='50px' alt='logo' />
+                    </Link>
+                    {
+                        location.pathname === '/register'?
+                        null
+                        :
+                        <>
+                            <span className='pl-10 font-bold'>
+                                Cards 
+                            </span>
+                            <span className='pl-10 font-bold'>
+                                <Link to='/menu'>
+                                    Order    
+                                </Link> 
+                            </span>
+                            <span className='pl-10 font-bold'>
+                                Gift
+                            </span>
+                        </>
+                    }
                 </div>
-              : (
-                <>
-                  <button className="my-bg-dark my-light rounded-full mr-2 px-3 py-2 m-5">
-                    <Link to="/login">Sign in</Link>
-                  </button>
-                  <button
-                    className="my-dark rounded-full px-3 py-2 m-5"
-                    style={{ border: "1px solid black" }}
-                  >
-                    <Link to="/register">Join now</Link>
-                  </button>
-                </>
-              )}
-            </>
-          )}
+                <div className='flex'>
+                    {
+                        location.pathname === '/register'?
+                        null
+                        :
+                        <>
+                            <div className='flex items-center'>
+                                <MdLocationOn />
+                                <span className='mr-10 font-bold'>
+                                    Find a store
+                                </span>
+                            </div>
+                            {
+                                props.data.username?
+                                    <div className='flex items-center'>
+                                        <div className='font-bold'>
+                                            {props.data.username} 
+                                        </div>
+                                        <div className='my-fs-20 mt-1 ml-1'>
+                                            <CgProfile onClick={props.logout.onLogout} />
+                                        </div>
+                                    </div>
+                                :
+                                    <>
+                                        <button className='my-bg-dark my-light rounded-full mr-3 px-3 py-2'>
+                                            <Link to='/login'>
+                                                Sign in
+                                            </Link>
+                                        </button>
+                                        <button className='my-dark rounded-full px-3 py-2' style={{border: '1px solid black'}}>
+                                            <Link to='/register'>
+                                                Join now
+                                            </Link>
+                                        </button>
+                                    </>
+                            }
+                        </>
+                    }
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
   );
 };
 
