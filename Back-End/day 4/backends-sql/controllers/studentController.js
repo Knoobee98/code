450145e8e4ca2a392b20a1b68c8264225b177485
Fauzan <55498447+Knoobee98@users.noreply.github.com>
2022-12-15 15:02:17 +0700

    day 4 done

--- Back-End/day 4/backends-sql/controllers/studentController.js
+++ Back-End/day 4/backends-sql/controllers/studentController.js
@@ -9,15 +9,40 @@ module.exports = {
             console.log(getData)
             res.status(201).send({ 
                 isError: false,
-                message: 'get all students success',
+                message: 'get data success',
                 data: getData
             })
         } catch (error) {
             res.status(500).send({
                 isError: true,
-                message: 'get all students failed to get data',
-                error: error.message
+                message: error.message,
+                data: null
             })
         }
-    }
+    },
+
+    addStudents: async(req, res) => {
+        try {
+            //step 1: get value dari req.body
+            let body = req.body
+            //step 2: insert data ke database
+            console.log(req.body)
+            await query('INSERT INTO students SET ?', body)
+    
+            //step 3: send response
+            res.status(201).send({ 
+                isError: false,
+                message: 'add data success',
+                data: null
+            })
+        } catch(error){
+            console.log(error)
+            res.status(404).send({ 
+                isError: true,
+                message: error.message,
+                data: null
+            })
+        }
+    },
+    
 }
--- Back-End/day 4/backends-sql/routers/studentRouter.js
+++ Back-End/day 4/backends-sql/routers/studentRouter.js
@@ -4,5 +4,6 @@ const Router = express.Router()
 const { studentController } = require('../controllers')
 
 Router.get('/get', studentController.getAllStudents)
+Router.post('/add', studentController.addStudents)
 
 module.exports = Router;
