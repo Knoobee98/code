41c57edc587fe7681d165f415a61e96e9fd9fa71
Fauzan <55498447+Knoobee98@users.noreply.github.com>
2023-01-12 14:08:38 +0700

    create-react-app

--- Back-End/day 12/controllers/productController.js
+++ Back-End/day 12/controllers/productController.js
@@ -13,7 +13,6 @@ module.exports = {
     createProduct: async(req,res) => {
         const t = await sequelize.transaction()
         try {
-            console.log('masukcontroller')
             let dataToCreate = JSON.parse(req.body.data)
 
             let postProducts = await products.create(dataToCreate, {transaction: t})
@@ -24,7 +23,6 @@ module.exports = {
                 pathToCreate.push({path: value.path, products_id: products_id})
             })
             let createProductImages = await products_images.bulkCreate(pathToCreate, {transaction: t, ignoreDuplicates: true})
-            console.log('masuk123')
             await t.commit()
             res.status(201).send({
                 isError: false,
@@ -42,5 +40,97 @@ module.exports = {
             })
         }
         
+    },
+    deleteProduct: async(req, res) => {
+        const t = await sequelize.transaction()
+        try {
+            //step 1: ambil id dari query params
+            let products_id = parseInt(req.params.id)
+            //step 2: ambil path image to delete, untuk kebutuhan delete file
+            let findAllImagePath = await products_images.findAll({
+                where: {
+                    products_id
+                },
+                transaction: t
+            })
+            //step 3: delete data di tabel products_images where products_id = id
+            await products_images.destroy({
+                where: {
+                    products_id
+                },
+                transaction: t
+            })
+            //step 4: delete data di tabel products where id = id
+            await products.destroy({
+                where: {
+                    id: products_id
+                },
+                transaction: t
+            })
+            //step 5: delete files
+            deleteFiles({images: findAllImagePath})
+            //step 5: response
+            await t.commit()
+            res.status(201).send({
+                isError: false,
+                message: 'deletion product success',
+                data: null
+            })
+        } catch (error) {
+            await t.rollback()
+            deleteFiles(req.files)
+            res.status(404).send({
+                isError: true,
+                message: error.message,
+                data: null
+            })
+        }
+        
+        
+    },
+    updateProduct: async(req, res) => {
+        const t = await sequelize.transaction()
+        try {
+            //ambil id dari params
+            let products_images_id = parseInt(req.params.products_images_id)
+            //ambil path image to delete, untuk kebutuhan delete file
+            let getImagePath = await products_images.findOne({
+                where: {
+                    id: products_images_id
+                },
+                transaction: t
+            })
+            console.log(getImagePath.dataValues.path)
+            //delete data di tabel products_images where products_id = id
+            let pathToDelete = []
+            pathToDelete.push(getImagePath)
+
+            //delete data di tabel products_images where products_id = id
+            await products_images.update({
+                path: pathToDelete[0].dataValues.path
+            }, {
+                where: {
+                    id: products_images_id
+                }
+            }, {transaction: t})
+            //delete files
+            deleteFiles({images: pathToDelete})
+            //response
+            await t.commit()
+            res.status(201).send({
+                isError: false,
+                message: 'update product success',
+                data: null
+            })
+        } catch (error) {
+            await t.rollback()
+            deleteFiles(req.files)
+            res.status(404).send({
+                isError: true,
+                message: error.message,
+                data: null
+            })
+        }
+
     }
 }
--- Back-End/day 12/routers/productRouter.js
+++ Back-End/day 12/routers/productRouter.js
@@ -5,5 +5,7 @@ const { productController } = require('../controllers')
 const upload = require('./../middleware/upload')
 
 router.post('/create', upload, productController.createProduct)
+router.delete('/delete/:id', productController.deleteProduct)
+router.patch('/update/:products_images_id', upload, productController.updateProduct)
 
 module.exports = router
